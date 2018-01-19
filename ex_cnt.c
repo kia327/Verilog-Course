fda32a6b19d41eaa6cf9ce44a8f27be18e8c3c9b
kia327 <35589368+kia327@users.noreply.github.com>
2018-01-20 02:28:53 +0800

    Create ex_cnt.c

---
+++ ex_cnt.c
@@ -0,0 +1,13 @@
+module ex_cnt(
+		input	wire				sclk,
+		input	wire				rst_n,
+		output	wire	[9:0]		cnt
+);
+reg		[9:0]		cnt_r;
+
+always @(posedge sclk or negedge rst_n)
+		if(rest_n == 1'b0)
+				cnt_r<=10'd0;//1023+1=1024
+		else 
+				cnt_r<=cnt_r + 1'b1;
+endmodule
