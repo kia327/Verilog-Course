af358d5eb8f853643348f26e22642e4324c8d459
kia327 <35589368+kia327@users.noreply.github.com>
2018-01-20 02:57:30 +0800

    Update ex_cnt.v

--- ex_cnt.v
+++ ex_cnt.v
@@ -7,7 +7,7 @@ reg		[9:0]		cnt_r;
 
 always @(posedge sclk or negedge rst_n)
 		if(rest_n == 1'b0)
-				cnt_r<=10'd0;//1023+1=1024, the last 10 bit turn to all 0
+				cnt_r<=10'd0;//1023+1=1024, the last 10 bit turn to 0s
 		else 
 				cnt_r<=cnt_r + 1'b1;
 endmodule
